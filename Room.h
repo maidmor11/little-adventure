94aec17fbc28b893fd192b13b476a190d8d2cf43
Jakob Leibetseder <jleibetseder4@gmail.com>
2018-05-17 21:03:37 +0200

    Added new rooms

--- Room.h
+++ Room.h
@@ -2,6 +2,7 @@
 #define ROOM_H
 
 #include <SFML/Graphics.hpp>
+
 class Engine;
 
 class State
@@ -14,33 +15,13 @@ public:
     CHOOSE_RACE,
     CHOOSE_CLASS,
     CHOOSE_ABILITIES,
-    ARENA};
+    ARENA
+  };
   virtual void enter(Engine* engine) = 0;
   virtual void exit() = 0;
-  virtual int transition() = 0;
+  virtual int transition(sf::RenderWindow* game_window) = 0;
 protected:
   // Engine* engine_;
   std::vector<sf::Sprite> room_sprite_vector_;
-};
-
-class Menu : public State
-{
-public:
-  void enter(Engine* engine);
-  void exit();
-  int transition(sf::RenderWindow* game_window);
-private:
-  //Objects in Room
-  sf::Texture plant_;
-  sf::Sprite plant_sprite_;
-};
-
-class Arena : public State
-{
-public:
-  void enter(Engine* engine);
-  void exit();
-  int transition(sf::RenderWindow* game_window);
-private:
 };
 #endif
