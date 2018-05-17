70abe9f6b98e5f7b508d46ff8923cb0d068fbcd8
Jakob Leibetseder <jleibetseder4@gmail.com>
2018-05-17 19:39:54 +0200

    Added Arena in its own class

---
+++ Arena.cpp
@@ -0,0 +1,35 @@
+#include "Arena.h"
+#include "Engine.h"
+#include <iostream>
+#include <vector>
+
+
+void Arena::enter(Engine* engine)
+{
+  this->room_sprite_vector_.clear();
+  arena_background_.loadFromFile("./assets/arena_background.png");
+  background_sprite_.setTexture(arena_background_);
+  this->room_sprite_vector_.push_back(background_sprite_);
+  engine->setSpriteVector(this->room_sprite_vector_);
+  std::cout << "Entered Arena" << std::endl;
+}
+
+void Arena::exit()
+{
+  std::cout << "Exit Arena" << std::endl;
+}
+
+int Arena::transition()
+{
+  int x;
+  std::cin >> x;
+  switch(x)
+  {
+    case MENU:
+      return current_room::MENU;
+    case ARENA:
+      return current_room::ARENA;
+    default:
+      return current_room::MENU;
+  }
+}
---
+++ Arena.h
@@ -0,0 +1,18 @@
+#ifndef ARENA_H
+#define ARENA_H
+#include "Room.h"
+
+class Engine;
+class Arena : public State
+{
+public:
+  void enter(Engine* engine);
+  void exit();
+  int transition();
+private:
+  //Background Image
+  sf::Texture arena_background_;
+  sf::Sprite background_sprite_;
+};
+
+#endif
