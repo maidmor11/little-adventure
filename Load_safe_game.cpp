7b5a50d7796defd632c249f9eee9420dd93c26c7
Max Strohmaier <max-strohmaier@hotmail.com>
2018-05-18 07:41:57 -0700

    ...

---
+++ Load_safe_game.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+
+#include "Load_safe_game.h"
+
+void Load::enter(Engine* engine)
+{
+  
+}
+
+void Load:exit()
+{
+  std::cout << "Exit Load Savegames" << std::endl;
+}
+
+void Load::transition(sf::RenderWindow* game_window)
+{
+
+}
--- Load_safe_game.h
+++ Load_safe_game.h
@@ -1,9 +1,17 @@
 #ifndef LOAD_SAVE_GAME_H
 #define LOAD_SAVE_GAME_H
 
-class LOAD
+class Player;
+
+class Load : public State
 {
-  
+public:
+  void enter(Engine* engine);
+  void exit();
+  int transition(sf::RenderWindow* game_window);
+private:
+  std::string read_save_;
+  Player* player_;
 };
 
 
